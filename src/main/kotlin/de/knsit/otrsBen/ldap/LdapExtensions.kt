48cbb76c852f9d1df3d7ad98fcd1f9e3a13eeb87
Peter Kurfer <peter.kurfer@googlemail.com>
2018-04-04 21:54:37 +0100

    Shortened LDAP extension

--- src/main/kotlin/de/knsit/otrsBen/ldap/LdapExtensions.kt
+++ src/main/kotlin/de/knsit/otrsBen/ldap/LdapExtensions.kt
@@ -1,20 +1,10 @@
 package de.knsit.otrsBen.ldap
 
-import javax.naming.NamingEnumeration
-import javax.naming.directory.Attribute
 import javax.naming.directory.Attributes
 
 /**
  * @author Peter Kurfer
  * Created on 4/3/18.
  */
-fun Attributes.toMap(): Map<String, Any> {
-    val resultMap: MutableMap<String, Any> = mutableMapOf()
-    val namingEnumeration: NamingEnumeration<out Attribute>? = all
-    while (namingEnumeration != null && namingEnumeration.hasMore()) {
-        val tmp = namingEnumeration.next()
-        resultMap[tmp.id] = tmp.get()
-    }
-
-    return resultMap
-}
+fun Attributes.toMap(): Map<String, Any> = all?.toList()?.groupBy { it.id }?.mapValues { it.value.first().get() }
+        ?: mapOf()
